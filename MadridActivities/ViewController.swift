b6db6f9f55113d64c07c42a5adbdaf4369230524
CarlosL <carlos.developing@gmail.com>
2017-09-19 15:42:17 +0200

    Commit de proyecto inicial

---
+++ MadridActivities/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MadridActivities
+//
+//  Created by Carlos Ledesma on 19/9/17.
+//  Copyright © 2017 Carlos Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
