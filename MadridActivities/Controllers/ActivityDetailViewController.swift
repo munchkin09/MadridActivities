2c7bdb9606f038660b8a8e7945a64b342067bf12
CarlosL <carlos.developing@gmail.com>
2017-09-28 16:10:12 +0200

    New gitignore for Pods files

---
+++ MadridActivities/Controllers/ActivityDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ActivityDetailViewController.swift
+//  MadridActivities
+//
+//  Created by Carlos Ledesma on 28/9/17.
+//  Copyright © 2017 Carlos Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class ActivityDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
